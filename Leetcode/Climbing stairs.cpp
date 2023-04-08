d636b0eba5d3ac403ff99f1473af69c1bd2db716
Shifa <121536886+ShifatJahanShifa@users.noreply.github.com>
2023-04-08 12:12:12 +0600

    Create Climbing stairs.cpp

---
+++ Leetcode/Climbing stairs.cpp
@@ -0,0 +1,28 @@
+problem link: https://leetcode.com/problems/climbing-stairs/ 
+
+
+                                                           solution
+ ----------------------------------------------------------------------------------------------------------------------------------------
+ class Solution {
+public:
+
+    int totalSteps(int stairIndex,vector<int>&DP) {
+    if(stairIndex==0) return 1;
+    if(DP[stairIndex]!=-1) return DP[stairIndex];
+
+    int left,right;
+    left=totalSteps(stairIndex-1,DP);
+    right=1;
+    if(stairIndex>1)
+    right=totalSteps(stairIndex-2,DP);
+
+    DP[stairIndex]=right+left;
+    return DP[stairIndex];
+
+    }
+
+    int climbStairs(int n) {
+        vector<int>DP(n,-1);
+        return totalSteps(n-1,DP);
+    }
+};
