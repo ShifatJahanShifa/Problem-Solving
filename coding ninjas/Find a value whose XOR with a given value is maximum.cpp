33aea419d48ba6b3ad51f061def5502f7528c076
Shifa <121536886+ShifatJahanShifa@users.noreply.github.com>
2023-04-20 17:42:38 +0600

    easy!

---
+++ coding ninjas/Find a value whose XOR with a given value is maximum.cpp
@@ -0,0 +1,9 @@
+problem link: https://www.codingninjas.com/codestudio/guided-paths/competitive-programming/content/148629/offering/1792856?leftPanelTab=0
+
+#include <bits/stdc++.h> 
+long long findValueWhoseXORWithGivenNumberIsMaximum(long long X)
+{
+	long long ans=0;
+	ans=X^((1LL<<61)-1);
+	return ans;
+}
