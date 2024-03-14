afb3e6bfa55109e413367dcb83a14591dcbc71c7
Shifa <121536886+ShifatJahanShifa@users.noreply.github.com>
2024-03-15 01:57:54 +0600

    greedy

---
+++ CSES/Increasing Array.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+int main()
+{
+    int n;cin>>n;
+    vector<int>v(n);
+    for(auto &it:v) cin>>it;
+    ll ans=0;
+    for (int i = 1; i <n; i++)
+    {
+        if(v[i]<v[i-1]){
+        ans+=v[i-1]-v[i];
+        v[i]=v[i-1];
+        }
+    }
+    cout<<ans<<"\n";
+    
+}
